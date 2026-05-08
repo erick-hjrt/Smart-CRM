11f3c13fbe0930505f0513091aa3d283fa6a169d
Nantenaina0107 <alfredandrianantenaina7@gmail.com>
2026-05-08 17:03:21 +0300

    refactor/ extraire la logique de scoring et réorganiser la structure de l'app

--- server/src/index.js
+++ server/src/index.js
@@ -2,5 +2,5 @@ const app = require("./app");
 const PORT = process.env.SERVER_PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(Démarrage de serveur sur le port : ${PORT});
+  console.log(`Démarrage de serveur sur le port : ${PORT}`);
 });
